6a496db104daf73c6f5c6bd071965855b1707bd8
Amer-N-Sulaiman <amersulaiman10@gmail.com>
2023-06-14 17:22:31 +0300

    Navs appear and disappear according to auth status

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,20 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {logout} from '../redux/features/userSlice'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 
 function MyNavbar() {
     const dispatch = useDispatch()
+    const user = useSelector(state=> state.user.user)
     return (
         <>
             <Navbar bg="dark" variant="dark" style={{marginBottom: '30px'}}>
             <Container>
                 <Navbar.Brand href="/">Quote Share</Navbar.Brand>
                 <Nav className="me-auto">
-                <Nav.Link href="/">Quotes Feed</Nav.Link>
-                <Nav.Link href="/login">Login</Nav.Link>
-                <Nav.Link href="/signup">Sign up</Nav.Link>
-                <Nav.Link onClick={()=>dispatch(logout())}>Log out</Nav.Link>
+                    {user && <Nav.Link href="/">Quotes Feed</Nav.Link>}
+                    {!user && <Nav.Link href="/login">Login</Nav.Link>}
+                    {!user && <Nav.Link href="/signup">Sign up</Nav.Link>}
+                    {user && <Nav.Link onClick={()=>dispatch(logout())}>Log out</Nav.Link>}
                 </Nav>
             </Container>
             </Navbar>
