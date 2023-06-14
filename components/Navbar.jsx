2a6342c6fed0b12c2ccedaf2eec6017ed61551cc
Amer-N-Sulaiman <amersulaiman10@gmail.com>
2023-06-14 12:50:36 +0300

    Added Nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,9 +9,9 @@ function MyNavbar() {
             <Container>
                 <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                 <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/signup">Sign Up</Nav.Link>
                 </Nav>
             </Container>
             </Navbar>
