fb730f5f89f2109156b05b4da1016542432725cd
Amer-N-Sulaiman <amersulaiman10@gmail.com>
2023-06-15 16:47:10 +0300

    Added Like Unlike Buttons

--- components/QuoteCard.jsx
+++ components/QuoteCard.jsx
@@ -1,8 +1,31 @@
 import Card from 'react-bootstrap/Card';
 import { useSelector } from 'react-redux';
+import {useState, useEffect} from 'react'
+import Button from '@mui/material/Button';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 
 function QuoteCard({quote}) {
   const user = useSelector(state=>state.user.user)
+  const [liked, setLiked] = useState(false)
+
+  useEffect(()=>{
+    if (!user){
+      return
+    }
+    setLiked(quote.likes.includes(user.username))
+  }, [user])
+
+  const handleLike = async()=>{
+    if (liked) {
+      setLiked(false)
+      // asyncthunk function to remove like
+    }
+    else {
+      setLiked(true)
+      // async function to add like
+    }
+  }
+
   return (
     <Card style={{marginTop: '40px'}}>
       <Card.Header>Uploaded by {user.full_name} ({user.username})</Card.Header>
@@ -16,6 +39,10 @@ function QuoteCard({quote}) {
           <footer className="blockquote-footer">
             <cite title="Source Title">{quote.author}</cite>
           </footer>
+          <Card.Footer>
+            {!liked && <Button onClick={handleLike} variant="outlined" startIcon={<ThumbUpAltIcon />}>Like</Button>}
+            {liked && <Button onClick={handleLike} variant="contained" startIcon={<ThumbUpAltIcon />}>Unlike</Button>}
+          </Card.Footer>
         </blockquote>
       </Card.Body>
     </Card>
